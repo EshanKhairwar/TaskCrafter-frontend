2eaab11601f5d8bedf006ed5da1a5d8e6467ed02
EshanKhairwar <eshanKhairwar@gmail.com>
2024-03-26 10:51:32 +0530

    first commit

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CatCard.scss";
+
+function CatCard({ card }) {
+  return (
+    <Link to="/gigs?cat=">
+      <div className="catCard">
+        <img src={card.img} alt="" />
+        <span className="title">{card.title}</span>
+        <span className="desc">{card.desc}</span>
+      </div>
+    </Link>
+  );
+}
+export default CatCard;
